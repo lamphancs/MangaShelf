88df8177e84158f1e3f01f6cab66d411fbf0198f
Lam Phan <lamphancs@gmail.com>
2026-04-24 18:11:13 -0700

    Initial Commit

---
+++ MangaShelf/MangaShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaShelfApp.swift
+//  MangaShelf
+//
+//  Created by Khoa Phan on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct MangaShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
